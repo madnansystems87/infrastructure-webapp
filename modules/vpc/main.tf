c3abafe04f3a4f82f757663d1c6ea89fe1d1d284
adnan <--global>
2025-04-12 22:49:19 +0500

    editing main and variables of vpc module files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,11 +11,11 @@ module "vpc"{
     create_database_subnet_route_table = true
     map_public_ip_on_launch = true
 
-    enable_nat_gateway = true
-    single_nat_gateway = true
+    #enable_nat_gateway = true
+    #single_nat_gateway = true
 
-    enable_dns_hostnames = true
-    enable_dns_support = true
+    #enable_dns_hostnames = true
+    #enable_dns_support = true
 
     tags = {
         Terraform = "true"
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,7 +1,7 @@
-variable "vpc_cidr_block" {
+#variable "vpc_cidr_block" {
 }
 
-variable "vpc_public_subnet_cidr_block" {
+#variable "vpc_public_subnet_cidr_block" {
 }
 
 variable "vpc_private_subnet_cidr_block"{
