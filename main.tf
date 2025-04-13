724998a771abd1956cfc83b136feb6c25eaabac1
adnan <--global>
2025-04-13 23:39:46 +0500

    update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.78.0"
+      version = "~> 5.78.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -13,8 +13,6 @@ terraform {
 
 provider "aws" {
   region = var.region
-
-
 }
 
 provider "kubernetes" {
