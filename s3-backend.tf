698ee5f8a57d40452ed08ed846f251dfba953bb8
adnan <--global>
2025-04-13 17:16:13 +0500

    Remove redundant AWS account ID variable from Terraform apply step in workflow

--- s3-backend.tf
+++ s3-backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "terraform-state-eks-productionenv"
     key            = "prod/terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-state-locked"
   }
 }
