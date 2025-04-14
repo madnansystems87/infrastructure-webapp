d71f8b8b0d653defafe414f8d3bc59dd43f06c1b
adnan <--global>
2025-04-14 17:59:29 +0500

    adding destroy infra workflow

--- modules/eks/configmap.tf
+++ modules/eks/configmap.tf
@@ -1,18 +1,18 @@
-data "template_file" "aws_auth" {
-  template = file("${path.module}/template/aws-auth.tpl")
+# data "template_file" "aws_auth" {
+#   template = file("${path.module}/template/aws-auth.tpl")
 
-  vars = {
-    node_role_arn   = var.node_role_arn
-    master_role_arn = var.master_role_arn
-    users           = jsonencode(var.users)
-  }
-}
+#   vars = {
+#     node_role_arn   = var.node_role_arn
+#     master_role_arn = var.master_role_arn
+#     users           = jsonencode(var.users)
+#   }
+# }
 
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+# resource "kubernetes_config_map" "aws_auth" {
+#   metadata {
+#     name      = "aws-auth"
+#     namespace = "kube-system"
+#   }
 
-  data = yamldecode(data.template_file.aws_auth.rendered)["data"]
-}
+#   data = yamldecode(data.template_file.aws_auth.rendered)["data"]
+# }
